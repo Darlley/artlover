7bfaf3419e3f0096a3f7371ed45e1712da1411db
Darlley <systembrtt@gmail.com>
2022-04-26 15:00:39 -0400

    add pest configuration

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -1,45 +1,32 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class AuthenticationTest extends TestCase
-{
-    use RefreshDatabase;
+test("test_login_screen_can_be_rendered")
+->get('/login')
+->assertOk();
 
-    public function test_login_screen_can_be_rendered()
-    {
-        $response = $this->get('/login');
+test('users can authenticate using the login screen', function(){
+    $user = User::factory()->create();
 
-        $response->assertStatus(200);
-    }
+    $response = $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ]);
 
-    public function test_users_can_authenticate_using_the_login_screen()
-    {
-        $user = User::factory()->create();
+    $this->assertAuthenticated();
+    $response->assertRedirect(RouteServiceProvider::HOME);
+});
 
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
+test('users can not authenticate with invalid password', function() {
+    $user = User::factory()->create();
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
-    }
+    $this->post('/login', [
+        'email' => $user->email,
+        'password' => 'wrong-password',
+    ]);
 
-    public function test_users_can_not_authenticate_with_invalid_password()
-    {
-        $user = User::factory()->create();
-
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'wrong-password',
-        ]);
-
-        $this->assertGuest();
-    }
-}
+    $this->assertGuest();
+});
