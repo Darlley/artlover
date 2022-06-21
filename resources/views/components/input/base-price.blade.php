93d20bfd41dbc96371acc4b522c6c854e2f3f453
Darlley <systembrtt@gmail.com>
2022-06-21 09:05:19 -0400

    Alpine Intro

--- resources/views/components/input/base-price.blade.php
+++ resources/views/components/input/base-price.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <input {{ $attributes->except(["wire:model"]) }} type="text" name="" id="price" inputmode="numeric" class="w-full placeholder-gray-400 border-0 outline-none ring-0 focus:ring-0 focus:outline-none bg-transparent p-0">
+<div 
+    x-data="{
+        name: 'Darlley',
+        handleClick(){ 
+            console.log(this.$refs.input)
+        }
+    }" 
+    x-init="console.log('this.$refs.input')"
+>
+    <input {{ $attributes->except(["wire:model"]) }} type="text" id="price" x-ref="input" inputmode="numeric" class="w-full placeholder-gray-400 border-0 outline-none ring-0 focus:ring-0 focus:outline-none bg-transparent p-0" @click="handleClick">
+
+    <span x-text="name"></span>
 </div>
