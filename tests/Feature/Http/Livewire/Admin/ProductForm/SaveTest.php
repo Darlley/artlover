707f165601c3c3d02011c8da0c7217c92adefdb1
Darlley <systembrtt@gmail.com>
2022-06-27 09:13:57 -0400

    save testes

--- tests/Feature/Http/Livewire/Admin/ProductForm/SaveTest.php
+++ tests/Feature/Http/Livewire/Admin/ProductForm/SaveTest.php
@@ -1,7 +1,12 @@
 <?php
 
-it('has http/livewire/admin/productform/save page', function () {
-    $response = $this->get('/http/livewire/admin/productform/save');
-
-    $response->assertStatus(200);
-});
+it('should be admin to save');
+it('should contain the name');
+it('should contain the description');
+it('if it has variation it must contain an image');
+it('if it has variation it must contain its name');
+it('if it has variation it must contain its price');
+it('if it has variation it must contain its quantity');
+it('if it has shipping it must contain its name');
+it('if it has shipping it must contain its standalone');
+it('if it has shipping it must contain its withothers');
