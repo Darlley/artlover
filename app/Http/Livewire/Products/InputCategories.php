aac12c2e9d4528698672658f3b2bae276834febc
Darlley <darlleybrito@gmail.com>
2022-09-16 09:08:58 -0400

    aula categories modal

--- app/Http/Livewire/Products/InputCategories.php
+++ app/Http/Livewire/Products/InputCategories.php
@@ -4,15 +4,24 @@ namespace App\Http\Livewire\Products;
 
 use App\Models\Product;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class InputCategories extends Component
 {
     public $product;
     public $isOpen;
+    public $categories = [];
 
     public function mount($product){
         // $this->product = $product;
         $this->isOpen = false;
+    }
+
+    public function addCategory(){
+        $this->categories[] = [
+            'id' => Str::random(),
+            'name' => '',
+        ];
     }
 
     public function render()
--- resources/views/livewire/products/input-categories.blade.php
+++ resources/views/livewire/products/input-categories.blade.php
@@ -6,41 +6,30 @@
 
     <div class="flex justify-center items-center absolute z-10 p-4 bg-black bg-opacity-50 w-full h-full top-0 left-0" wire:model='isOpen'>
         <div class="w-max flex flex-col">
-            <ul class="flex flex-col bg-white rounded-md overflow-hidden font-bold text-gray-600 min-w-[320px]">
-                <li class="flex gap-2">
-                    <button class="flex py-4 px-8 w-full text-cyan-500 bg-blue-200 gap-8">
-                        <x-icon.check class="w-6" />
-                        <span>Category 1</span>
-                    </button>
-                </li>
-                <li class="flex gap-2">
-                    <button class="flex py-4 px-8 w-full gap-8">
-                        <span>Category 1</span>
-                    </button>
-                </li>
-                <li class="flex gap-2">
-                    <button class="flex py-4 px-8 w-full gap-8">
-                        <span>Category 1</span>
-                    </button>
-                </li>
-                <li class="flex gap-2">
-                    <button class="flex py-4 px-8 w-full gap-8">
-                        <span>Category 1</span>
-                    </button>
-                </li>
-                <li class="flex gap-2">
-                    <button class="flex py-4 px-8 w-full text-cyan-500 bg-blue-200 gap-8">
-                        <x-icon.check class="w-6" />
-                        <span>Category 1</span>
-                    </button>
-                </li>
-                <li class="flex gap-2">
-                    <button class="flex py-4 px-8 w-full gap-8">
-                        <span>Category 1</span>
-                    </button>
-                </li>
+            <div class="bg-white rounded-md overflow-hidden font-bold text-gray-600 min-w-[320px]">
+                <ul class="flex flex-col">
+                    @foreach ($categories as $category)    
+                    <li class="flex gap-2">
+                        <button class="flex py-4 px-8 w-full text-cyan-500 bg-blue-200 gap-8">
+                            <x-icon.check class="w-6" />
+                            <span>Category 1</span>
+                            <x-icon.trash class="text-red-500 w-5 h-5 opacity-80 hover:opacity-100 ml-auto" />
+                        </button>
+                    </li>
+                    <li class="flex gap-2">
+                        <button class="flex py-4 px-8 w-full gap-8">
+                            <span>Category 1</span>
+                            <x-icon.trash class="text-red-500 w-5 h-5 opacity-80 hover:opacity-100 ml-auto" />
+                        </button>
+                    </li>
+                    @endforeach
+                </ul>
 
-            </ul>
+                <x-base-button wire:click='addCategory' class="py-4 px-8 w-full flex items-center gap-2 bg-gray-100 hover:bg-gray-200">
+                    <x-icon.plus class="w-6 h-6" />
+                    <span>Adicionar Categoria</span>
+                </x-base-button>
+            </div>
             
             <div class="flex justify-center w-full text-center mt-4 gap-2">
                 <button wire:prevent wire:click="$toggle('isOpen')" class="p-2 w-full bg-white rounded-md text-cyan-500 font-bold opacity-90 hover:opacity-100">
